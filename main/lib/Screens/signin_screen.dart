13fe8077f4b330eecad0247fb6920c87711c4ef3
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-10-13 23:07:30 +0300

    add: Signin Screen

---
+++ main/lib/Screens/signin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SinginScreen extends StatefulWidget {
+  const SinginScreen({super.key});
+
+  @override
+  State<SinginScreen> createState() => _SinginScreenState();
+}
+
+class _SinginScreenState extends State<SinginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
